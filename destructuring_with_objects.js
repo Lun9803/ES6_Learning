f69be8a19df7528c1ce0b0d7d5ce20ff3cc02ba5
Bolun Li <boli7059@uni.sydney.edu.au>
2019-02-27 10:44:39 +1100

    finished destructuring with objects

--- destructuring_with_objects.js
+++ destructuring_with_objects.js
@@ -49,4 +49,57 @@ var [a] = 'hello';
 console.log('\n6.');
 console.log(a);
 
-// to be continued...
+// 7. 因为各种变量都有自身属性，如toString, prop，所以也可以当作key
+// 一下例子中存储到变量s的值是名为toString的function
+var {toString: s} = 500;
+console.log('\n7.');
+console.log(s);
+
+// 8. 函数参数的解构赋值-数组
+function sample([a, [b, c]]){
+    return a + b + c;
+}
+
+console.log('\n8.');
+console.log(sample([1, [2, 3]]));
+// 下例不匹配，显示undefined
+console.log(sample[1, 3, 3]);
+
+// 可以给参数设置默认值
+function sample_2([a=3, [b=3, c=3]]){
+    return a + b + c;
+}
+// 不完全解构时默认值才生效，否则报错
+// 如：传入参数是[1, 3]则报错
+console.log(sample_2([1, [3]]));
+
+// 9. 函数参数解构赋值-对象
+// 重点：以下两种方法结果不一样
+
+// 第一个函数是为x和y指定默认值，如果结构失败就赋默认值
+function showVar({x = 0, y = 0} = {}){
+    console.log('showVar:', [x, y]);
+}
+// 这里是为参数{x, y}指定默认值
+function showVar_2({x, y} = {x:0, y:0}){
+    console.log('showVar_2:', [x, y]);
+}
+// 无参数的情况都会返回默认指定的
+showVar();
+showVar_2();
+// 传入空对象的时候，第一个函数解构失败，x和y被指定默认值
+// 第二个函数接受到了对象类型的参数，所以参数的默认值不会生效，接下来会解构失败，所以x和y是undefined
+showVar({});
+showVar_2({});
+// 和上面类似，第一个函数内y的值解构失败，所以被赋予默认值
+// 而第二个函数依旧不会触发参数默认值，x可以取到值而y是undefined
+showVar({x:1});
+showVar_2({x:1});
+
+// 解构赋值的用途
+// 1. 交换变量值， [x, y] = [y, x]
+// 2. 函数返回对象，通过解构赋值直接获取多个值
+// 3. 提取json
+// 4. 参数定义
+// 5. 函数参数默认值
+// 6. 遍历map结构
