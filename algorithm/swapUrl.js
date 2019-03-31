039878e12fd1fc6bc3aec4af837d036331584843
Lun9803 <bolunlun9805@163.com>
2019-03-31 17:43:35 +1100

    finished swap url

--- algorithm/swapUrl.js
+++ algorithm/swapUrl.js
@@ -13,20 +13,29 @@ function swapUrlTotal(url){
 console.log(swapUrlTotal("www.baidu.com"));
 console.log(swapUrlTotal("www.baidu.com/image/photos"));
 
-
 // 只把顺序反转 www.baidu.com -> com.baidu.www
-function swapUrlPartial(url){
-    let reg = /(\S*)[.](\w*)/;
-    let regFindDot = /[.]/;
-
-    if(!regFindDot.test(url))return url;
-
-    let subStr = "";
-    for(let i=url.length; i>=0; i--){
-        if(url.charAt(i)==".")subStr=url.slice(0, i);
+function findNextDotIndex(url, startIndex){
+    for(let i=startIndex-1; i>=0; i--){
+        if(i==0)return 0;
+        if(url.charAt(i)=='.')return i;
     }
-    
-    return url.replace(reg, `$2.`+swapUrlPartial(subStr));
+}
+
+function swapUrlPartial(url){
+    let originalLength = url.length;
+    let startIndex = url.length;
+    while(true){
+        let nextDotIndex = findNextDotIndex(url, startIndex);
+        if(nextDotIndex!=0){
+            url = url.concat(".", url.slice(nextDotIndex+1, startIndex));
+        }
+        if(nextDotIndex==0){
+            url = url.concat(".", url.slice(nextDotIndex, startIndex));
+            break;
+        }
+        startIndex = nextDotIndex;
+    }
+    return url.slice(originalLength+1, url.length);
 }
 
 console.log(swapUrlPartial("www.baidu.com.123.321"));
